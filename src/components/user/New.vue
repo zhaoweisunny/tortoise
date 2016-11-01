4834dd83c615b8a237f36bca298536baa3a51346
songjiebo <642945852@qq.com>
2016-11-01 19:44:59 +0800

    add New.vue

---
+++ src/components/user/New.vue
@@ -0,0 +1,30 @@
+/**
+* Created by Administrator on 2016/11/1.
+*/
+
+<template>
+  <div class="New">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'New',
+    data () {
+      return {
+        msg: 'hello'
+      }
+    }
+  }
+</script>
+
+
+<style rel="stylesheet/less" type="text/css">
+  @import "../../assets/css/variable.less";
+
+  .New {
+
+  }
+</style>
+
